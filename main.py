53897cc5f4c304cdbfbe5a0fca009957439f66a8
Hesoyam117 <81217165+Hesoyam117@users.noreply.github.com>
2022-09-04 10:06:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,25 +6,33 @@ from settings import *
 file = open("file.txt", "r")
 listmain = np.loadtxt("file.txt", delimiter='\t', dtype=np.float)
 num = len(listmain) // 10
-print('Найдено ', num, ' наборов данных, выберите сколько отобразить:\n 1-3: отобразить один из графиков\n 4: отобразить все ')
+print('Найдено ', num, ' наборов данных, выберите сколько отобразить:\n 1-3: отобразить один из графиков')
 choice = int(input())
 if choice == 1:
     list1 = listmain[:10]
+    list2 = list1[5:10]
+    list1 = list1[:5]
     print(list1)
+    print(list2)
 elif choice == 2:
     list1 = listmain[10:20]
+    list2 = list1[5:10]
+    list1 = list1[:5]
     print(list1)
+    print(list2)
 elif choice == 3:
     list1 = listmain[20:30]
+    list2 = list1[5:10]
+    list1 = list1[:5]
     print(list1)
-elif choice == 4:
-    list1 = listmain
-    print(list1)
+    print(list2)
+else:
+    print('Ошибка')
 
 
-list2 = [0] * len(list1)
-for i in range (0, len(list1)):
-    list2[i] = np.cos(list1[i])
+#list2 = [0] * len(list1)
+#for i in range (0, len(list1)):
+    #list2[i] = np.cos(list1[i])
 
 #func = pd.DataFrame({'x': list1, 'y': list2})
 #plt.plot(func['x'], func['y'])
