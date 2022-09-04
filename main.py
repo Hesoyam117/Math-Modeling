ab37eaa0c754477582df6a36afc4b2debc2a56cb
Hesoyam117 <81217165+Hesoyam117@users.noreply.github.com>
2022-09-04 09:53:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,11 +4,27 @@ import numpy as np
 from settings import *
 
 file = open("file.txt", "r")
-#list1 = np.loadtxt("file.txt", delimiter='\t', dtype=np.float)
-list1 = file.readline()
+listmain = np.loadtxt("file.txt", delimiter='\t', dtype=np.float)
+num = len(listmain) // 10
+print('Найдено ', num, ' наборов данных, выберите сколько отобразить:\n 1-3: отобразить один из графиков\n 4: отобразить все ')
+choice = int(input())
+if choice == 1:
+    list1 = listmain[:10]
+    print(list1)
+elif choice == 2:
+    list1 = listmain[10:20]
+    print(list1)
+elif choice == 3:
+    list1 = listmain[20:30]
+    print(list1)
+elif choice == 4:
+    list1 = listmain
+    print(list1)
+
+
 list2 = [0] * len(list1)
 for i in range (0, len(list1)):
-    list2[i] = float(list1[i] ** 2)
+    list2[i] = np.cos(list1[i])
 
 #func = pd.DataFrame({'x': list1, 'y': list2})
 #plt.plot(func['x'], func['y'])
